a15d9148ea37e4cd5da1c648f1bc11b5ea1168fb
hamzacr7 <hamzahaslam523@gmail.com>
2023-01-16 16:02:41 +0530

    route programatically

--- src/app/components/profile/my-profile/my-profile.component.ts
+++ src/app/components/profile/my-profile/my-profile.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-profile',
   templateUrl: './my-profile.component.html',
   styleUrls: ['./my-profile.component.css']
 })
-export class MyProfileComponent {
+export class MyProfileComponent implements OnInit {
 
-  constructor() { 
+  constructor(private router:Router) { 
     console.log("My Profile is working..");
     
   }
 
-  // ngOnInit(): void {
-  // }
+  ngOnInit(): void {
+  }
+  onBtnClick(): void {
+    this.router.navigate(['/']);
+  }
 
 }

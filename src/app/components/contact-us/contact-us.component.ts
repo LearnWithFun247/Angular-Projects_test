7825f7bcc7129da6e2769b73f09b57e5729a4c79
hamzacr7 <hamzahaslam523@gmail.com>
2023-02-02 22:29:00 +0530

    custom validators synchronous and asynchronous,password basic validations,validation on checkbox

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit ,OnChanges, SimpleChanges} from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AsyncValidatorFn, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-contact-us',
@@ -16,7 +17,8 @@ notAllowedNames=['Name','UserName','username','name','userName','Username']
     this.contactUsForm=new FormGroup({
       'userDetails':new FormGroup({
         'name':new FormControl('',[Validators.required,this.invalidNames.bind(this)]),
-        'email':new FormControl('',[Validators.required,Validators.email]),  
+        'email':new FormControl('',[Validators.required,Validators.email],this.invalidEmail as AsyncValidatorFn),
+        'password':new FormControl('',[Validators.required,Validators.maxLength(6)])  
       }),
       'cars':new FormControl('BMW',Validators.required),
       'gender':new FormControl('MALE',Validators.required),
@@ -37,6 +39,19 @@ notAllowedNames=['Name','UserName','username','name','userName','Username']
       return {errorMsg:true}
     }
     return null
+  }
+  invalidEmail(controls:FormControl):Promise<any> | Observable<any>{
+    let emailResponse=new Promise((res,rej)=>{
+      setTimeout(()=>{
+        if(controls.value==='abc@aa.com'){
+          res ({errorMsg:true})
+        }else{
+          res(null)
+        }
+      },2000)
+    })
+    return emailResponse
+    
   }
 
 }
